a10805a33b20001960caea80e236830d2a9a30ad
Dope-Otaku <fakeyt909@gmail.com>
2024-12-13 22:47:53 +0530

    conflict branch opened

--- version1.py
+++ version1.py
@@ -2,6 +2,7 @@ version_number = 11
 message = 'this time its the difference between two different branches! [change]'
 
 branch = "b1"
+conflict = "exp"
 
 
 print(f"{version_number}: {message} --> in Branch:{branch}")
