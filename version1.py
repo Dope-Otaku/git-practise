b5a8d227582043f9277d82ed5f4f20074f0a50ba
Dope-Otaku <fakeyt909@gmail.com>
2024-12-13 20:01:00 +0530

    local repo vs remote repo

--- version1.py
+++ version1.py
@@ -1,5 +1,5 @@
-version_number = 4
-message = 'this time its the fourth version! [change]'
+version_number = 11
+message = 'this time its the difference between local and remote changes version! [change]'
 
 
 print(f"{version_number}: {message}")
