fa1836741be2efca538e8dfb36f433fd7f6ed84b
Dope-Otaku <fakeyt909@gmail.com>
2024-12-13 19:12:07 +0530

    Version 4 added

--- version1.py
+++ version1.py
@@ -1,5 +1,5 @@
-version_number = 3
-message = 'this time its the third version! [change]'
+version_number = 4
+message = 'this time its the fourth version! [change]'
 
 
 print(f"{version_number}: {message}")
